3cb878e1c14e5d921e06baf03605e39b0d99e195
Yahya <husain.host@gmail.com>
2018-04-05 17:51:02 +0300

    Added test for user-registration if successful , passwordhashed and user added

--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,5 @@
 import unittest
-from data import User
+from data import User, user_data
 from app.views import app
 
 
@@ -9,7 +9,9 @@ class TestUser(unittest.TestCase):
         app.testing = True
         app.config['WTF_CSRF_ENABLED'] = False
         self.app = app
-        app.michael = User('Michael', 'Johnson', 'user_1@gmail.com', 'password2018')
+        self.client = app.test_client()
+        self.michael = User('Michael', 'Johnson', 'user_1@example.com', 'password2018')
+        self.johndoe = User('john', 'Doe', 'user_2@example.com', 'password2018')
 
     def test_application_running(self):
         """ensures that flask was setup correctly"""
@@ -22,3 +24,18 @@ class TestUser(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/login', content_type='html/text')
         self.assertTrue(b'PLEASE LOGIN' in response.data)
+
+    def test_user_registration_successful(self):
+        """test for successful registration"""
+        self.assertTrue(user_data[hash('user_1@example.com')]['Email'], 'user_1@gmail.com')
+
+    def test_user_password_hash(self):
+        """test if user password is encrypt"""
+        self.assertNotEqual(user_data[hash('user_1@example.com')]['Password'], 'password218')
+
+    def test_user_added(self):
+        """test if user is added"""
+        self.assertIsInstance(self.michael, User, False)
+
+
+
